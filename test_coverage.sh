5acb728148110b2c3c10cdfee11ca1b9d5b5a83f
sylvain chassang <sylvain.chassang@gmail.com>
2019-03-07 11:59:02 -0500

    test coverage should exclude scripts

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-coverage run --source=missing_bids/ --omit=*/tests/*,*__init__.py \
+coverage run --source=missing_bids/ --omit=*/tests/*,*__init__.py,*/scripts/* \
 -m py.test missing_bids/tests/
 
 coverage report -m
