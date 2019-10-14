a6907da85b0d8cd4c384b82c4e18e5895650bea0
sylvain <sylvain.chassang@gmail.com>
2019-10-14 16:54:47 -0400

    parallel solver

--- tests/monotonicity_issue.py
+++ tests/monotonicity_issue.py
@@ -22,6 +22,7 @@ multistage_pc_data_after = rb.RefinedMultistageData(
 multistage_demands_pc_after = \
     multistage_pc_data_after.assemble_target_moments(deviations)
 
+print('>>> iterated solver')
 for m_0, seed in product(m_array, [0, 1, 2]):
     constraints = [
         environments.MarkupConstraint(max_markup=.5, min_markup=m_0)]
@@ -36,6 +37,30 @@ for m_0, seed in product(m_array, [0, 1, 2]):
         project=True,
         filter_ties=None,
         number_iterations=25,
+        confidence_level=.95,
+        moment_matrix=rb.refined_moment_matrix(),
+        moment_weights=np.identity(5)
+    )
+    min_collusion_solver.max_best_sol_index = 100
+    share = min_collusion_solver.result.solution
+    print('min markup {}, seed {} | share non collusive: {}'.format(
+        m_0, seed, 1 - share))
+
+print('>>> parallel solver')
+for m_0, seed in product(m_array, [0, 1, 2]):
+    constraints = [
+        environments.MarkupConstraint(max_markup=.5, min_markup=m_0)]
+
+    min_collusion_solver = rb.ParallelRefinedMultistageSolver(
+        data=multistage_pc_data_after,
+        deviations=deviations,
+        metric=rb.RefinedMultistageIsNonCompetitive,
+        plausibility_constraints=constraints,
+        num_points=1000.0,
+        seed=seed,
+        project=True,
+        filter_ties=None,
+        number_evaluations=25,
         confidence_level=.95,
         moment_matrix=rb.refined_moment_matrix(),
         moment_weights=np.identity(5)
