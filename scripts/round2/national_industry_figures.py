16d7cd658058af2d4917efa94ab5a6a0ebe230f4
sylvain <sylvain.chassang@gmail.com>
2019-11-01 14:18:04 -0400

    national industry

--- scripts/round2/national_industry_figures.py
+++ scripts/round2/national_industry_figures.py
@@ -1,33 +1,39 @@
 from scripts.figures_import_helper import *
 
-# before/after industry comparisons, using national data
+# before/after collusion scandal industry comparisons, using national data
 
 list_data_sets = [
-    ('Bridges', 'bc_collusion.csv', [-.025, .0, .001]),
-    ('Electric', 'ec_collusion.csv', [-.025, .0, .001]),
-    ('Pre-Stressed Concrete', 'pc_collusion.csv', [-.025, .0, .001]),
-    ('Floods', 'fc_collusion.csv', [-.03, .0, .001])
+    ('Bridges', 'bc_collusion.csv'),
+    ('Electric', 'ec_collusion.csv'),
+    ('Pre-Stressed Concrete', 'pc_collusion.csv'),
+    ('Floods', 'fc_collusion.csv')
 ]
 
-for industry, file, deviations in list_data_sets:
+deviations = [-.025, .0, .001]
+for industry, file in list_data_sets:
     print('='*20 + '\n' + industry)
     print('collecting and processing data')
-    data = auction_data.AuctionData(os.path.join(path_data, file))
-    data_before = auction_data.AuctionData(
+    data = rebidding.RefinedMultistageData(
+        os.path.join(path_data, file))
+    data_before = rebidding.RefinedMultistageData.from_clean_bids(
         data.df_bids.loc[data.data.before == 1])
-    data_after = auction_data.AuctionData(
+    data_after = rebidding.RefinedMultistageData.from_clean_bids(
         data.df_bids.loc[data.data.before.isnull()])
+    plot_delta(data=data_before, filename='R2/{}_delta_before'.format(industry))
+    plot_delta(data=data_after, filename='R2/{}_delta_after'.format(industry))
 
     print('computing before/after problem solution')
-    solutions_before, _ = compute_minimization_solution_unfiltered(
+    solutions_before, _ = compute_solution_rebidding(
         data_before, deviations)
-    solutions_after, _ = compute_minimization_solution_unfiltered(
+    solutions_after, _ = compute_solution_rebidding(
         data_after, deviations)
     del data
     del data_before
     del data_after
 
     print('saving plot\n')
-    pretty_plot(os.path.join('R1', industry),
+    pretty_plot(os.path.join('R2', industry),
                 np.array([1 - solutions_before, 1 - solutions_after]),
                 np.array(["before investigation", "after investigation"]))
+
+
