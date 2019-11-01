b38d86ece0f66405877ecc8243d780011d4154b3
sylvain <sylvain.chassang@gmail.com>
2019-11-01 13:09:00 -0400

    individual firms

---
+++ scripts/round2/city_level_individual_firms.py
@@ -0,0 +1,54 @@
+from scripts.figures_import_helper import *
+# %matplotlib inline
+
+# before/after industry comparisons, using national data
+
+filename = 'municipal_pub_reserve_no_pricefloor.csv'
+
+# +
+filter_ties = auction_data.FilterTies(tolerance=.0001)
+
+data = filter_ties(
+    auction_data.AuctionData(os.path.join(path_data, filename)))
+plot_delta(data)
+# -
+
+data.df_bids.head()
+
+num_auctions_by_bidder = data.data.groupby('bidder_id').size()
+top30_bidders = num_auctions_by_bidder.sort_values(ascending=False).head(30)
+top30_bidders = top30_bidders.index
+top30_bidders
+
+deviations = [-.025, .0, .001]
+list_solutions = []
+for i, bidder in enumerate(top30_bidders):    
+    print('firm {}'.format(i + 1))
+    data_firm = auction_data.AuctionData.from_clean_bids(
+        data.df_bids.loc[data.data.bidder_id == bidder])
+    demand_firm = data_firm.assemble_target_moments(deviations)
+    constraints = [environments.MarkupConstraint(max_markup=.5, min_markup=.05)]
+
+    min_collusion_solver = solvers.ParallelSolver(
+        data=data_firm,
+        deviations=deviations,
+        metric=analytics.IsNonCompetitive,
+        plausibility_constraints=constraints,
+        num_points=NUM_POINTS,
+        seed=0,
+        project=False,
+        filter_ties=None,
+        num_evaluations=NUM_EVAL,
+        confidence_level=1 - .05 / len(deviations),
+        moment_matrix=auction_data.moment_matrix(deviations, 'slope'),
+        moment_weights=np.identity(len(deviations))
+    )
+    
+    list_solutions.append([i, bidder, min_collusion_solver.result.solution])
+
+print('saving data\n')
+save2frame(list_solutions,
+           ['rank', 'bidder_id', 'share competitive'],
+           'R2/city_auctions_individual_firms')
+
+
