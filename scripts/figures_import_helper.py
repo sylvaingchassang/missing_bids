a1d271f0c3f9f24c6b9c4712027708b61d5df18a
sylvain <sylvain.chassang@gmail.com>
2019-12-17 11:26:44 -0500

    sim params

--- scripts/figures_import_helper.py
+++ scripts/figures_import_helper.py
@@ -47,8 +47,8 @@ ensure_dir(os.path.join(path_figures, 'R2'))
 
 
 # set global optimization parameters
-NUM_POINTS = 3000
-NUM_EVAL = 5
+NUM_POINTS = 10000
+NUM_EVAL = 500
 
 
 def markup_info_constraints(max_markups, ks, demands):
