dfef9c68a97b7fff9c5dd58d9cbb2a354e578191
sylvain <sylvain.chassang@gmail.com>
2020-08-01 10:28:08 -0400

    simulate all

--- scripts/round3/generate_figures.sh
+++ scripts/round3/generate_figures.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-#python -m tsuchiura_figures
-#pkill python
-#python -m city_level_individual_firms
-#pkill python
-#python -m city_level_high_low
-#pkill python
-#python -m city_level_all
-#pkill python
-#python -m city_level_ex_tsuchiura
-#pkill python
-#python -m city_level_high_low_ex_tsuchiura
-#pkill python
-#python -m cities_high_case_study
-#pkill python
-
-#python -m national_sample
-#pkill python
+python -m tsuchiura_figures
+pkill python
+python -m city_level_individual_firms
+pkill python
+python -m city_level_high_low
+pkill python
+python -m city_level_all
+pkill python
+python -m city_level_ex_tsuchiura
+pkill python
+python -m city_level_high_low_ex_tsuchiura
+pkill python
+python -m cities_high_case_study
+pkill python
+python -m national_sample
+pkill python
 
 python -m national_sample_robustness
 pkill python
