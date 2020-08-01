f004ba5e589787033a58e83ae8c3c904ffc2a2e2
sylvain <sylvain.chassang@gmail.com>
2020-08-01 04:39:20 -0400

    example multistage moment matrices

--- scripts/round3/notebooks/debugging.py
+++ scripts/round3/notebooks/debugging.py
@@ -4,112 +4,115 @@ national_data = asymptotics.MultistagePIDMeanAuctionData(
     os.path.join(path_data, 'sample_with_firm_rank.csv'))
 
 # +
-# ms_moment_matrix = np.array([
-#     [-1, 0, 0, 0, 0],
-#     [0, 0, 0, 0, -1],
-#     [-1, 1, 1, 1, 0],
-#     [0, 0, 0, 1, -1]
-# ])
-# metric = rebidding.EfficientMultistageIsNonCompetitive
-# metric.max_markup = .5
-# metric.min_markup = .025
-#
-# deviations = [-.02, 0, .001]
-# solver = asymptotics.ParallelAsymptoticMultistageSolver(
-#     data=national_data,
-#     deviations=deviations,
-#     metric=metric,
-#     plausibility_constraints=[environments.EmptyConstraint()],
-#     num_points=500,
-#     num_evaluations=2,
-#     seed=0,
-#     project=False,
-#     filter_ties=None,
-#     confidence_level=[.999, .999, .974, .974],
-#     moment_matrix=ms_moment_matrix,
-#     enhanced_guesses=True
-# )
-#
-# print('demands', solver.get_solver(0).demands)
-#
-# print('tolerance', solver.get_solver(0).tolerance)
-#
-# print('solution', solver.result.solution)
-
-# +
 ms_moment_matrix = np.array([
-    [-1, 0, 0, 0, 0],
-    [-1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0]
+    [-.98, 1, .5, 1, 0],
+    [1, -1, -.5, -1, 0],
+    [0, 0, 0, 0, -1],
+    [0, 0, 0, 1, -1]
 ])
+
 metric = rebidding.EfficientMultistageIsNonCompetitive
 metric.max_markup = .5
 metric.min_markup = .025
 
-deviations = [-.02, 0, 1e-9]
-this_metric = metric(deviations)
+deviations = [-.02, 0, .001]
+solver = asymptotics.ParallelAsymptoticMultistageSolver(
+    data=national_data,
+    deviations=deviations,
+    metric=metric,
+    plausibility_constraints=[environments.EmptyConstraint()],
+    num_points=500,
+    num_evaluations=2,
+    seed=0,
+    project=False,
+    filter_ties=None,
+    confidence_level=[.999, .999, .974, .974],
+    moment_matrix=ms_moment_matrix,
+    enhanced_guesses=True
+)
 
-this_metric([0.858314, 0.047174, 0.001325, 0.755082, 0.698057, 0.353535])
+print('demands', solver.get_solver(0).demands)
 
-# solver = asymptotics.ParallelAsymptoticMultistageSolver(
-#     data=national_data,
-#     deviations=deviations,
-#     metric=metric,
-#     plausibility_constraints=[environments.EmptyConstraint()],
-#     num_points=500,
-#     num_evaluations=2,
-#     seed=0,
-#     project=False,
-#     filter_ties=None,
-#     confidence_level=[.999,  .951],
-#     moment_matrix=ms_moment_matrix,
-#     enhanced_guesses=True
-# )
-#
-# print('demands', solver.get_solver(0).demands)
-#
-# print('tolerance', solver.get_solver(0).tolerance)
-#
-# print('solution', solver.result.solution)
+print('tolerance', solver.get_solver(0).tolerance)
+
+print('solution', solver.result.solution)
 
 # +
-# ms_moment_matrix = np.array([
-#     [0, 0, 0, 0, -1],
-#     [0, 0, 0, 1, -1]
-# ])
-# metric = rebidding.EfficientMultistageIsNonCompetitive
-# metric.max_markup = .5
-# metric.min_markup = .025
-#
-# deviations = [-1e-9, 0, .001]
-# solver = asymptotics.ParallelAsymptoticMultistageSolver(
-#     data=national_data,
-#     deviations=deviations,
-#     metric=metric,
-#     plausibility_constraints=[environments.EmptyConstraint()],
-#     num_points=500,
-#     num_evaluations=2,
-#     seed=0,
-#     project=False,
-#     filter_ties=None,
-#     confidence_level=[.999, .951],
-#     moment_matrix=ms_moment_matrix,
-#     enhanced_guesses=True
-# )
-#
-# print('demands', solver.get_solver(0).demands)
-#
-# print('tolerance', solver.get_solver(0).tolerance)
-#
-# print('solution', solver.result.solution)
+ms_moment_matrix = np.array([
+    [-.98, 1, .5, 1, 0],
+    [1, -1, -.5, -1, 0]
+])
+metric = rebidding.EfficientMultistageIsNonCompetitive
+metric.max_markup = .5
+metric.min_markup = .15
+
+deviations = [-.02, 0, 1e-9]
+solver = asymptotics.ParallelAsymptoticMultistageSolver(
+    data=national_data,
+    deviations=deviations,
+    metric=metric,
+    plausibility_constraints=[environments.EmptyConstraint()],
+    num_points=100,
+    num_evaluations=2,
+    seed=0,
+    project=False,
+    filter_ties=None,
+    confidence_level=.95,
+    moment_matrix=ms_moment_matrix,
+    enhanced_guesses=True
+)
+
+print('demands', solver.get_solver(0).demands)
+
+print('tolerance', solver.get_solver(0).tolerance)
+
+print('solution', solver.result.solution)
 # -
 
+argmin = solver.result.argmin
+
+argmin
+
+# +
+ms_moment_matrix = np.array([
+    [0, 0, 0, 0, -1],
+    [0, 0, 0, 1, -1]
+])
+metric = rebidding.EfficientMultistageIsNonCompetitive
+metric.max_markup = .1
+metric.min_markup = .025
+
+deviations = [-1e-9, 0, .001]
+solver = asymptotics.ParallelAsymptoticMultistageSolver(
+    data=national_data,
+    deviations=deviations,
+    metric=metric,
+    plausibility_constraints=[environments.EmptyConstraint()],
+    num_points=500,
+    num_evaluations=2,
+    seed=0,
+    project=False,
+    filter_ties=None,
+    confidence_level=[.999, .951],
+    moment_matrix=ms_moment_matrix,
+    enhanced_guesses=True
+)
+
+print('demands', solver.get_solver(0).demands)
+
+print('tolerance', solver.get_solver(0).tolerance)
+
+print('solution', solver.result.solution)
+# -
+
+1-.001 * .114/(.0012)
+
+1/1.1
 
 
+1-.136
 
 
-
-
-
+1/1+.1"
 
 
