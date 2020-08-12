cb3246122b3fe2f420ce3850b39624bff54ab0c7
sylvain <sylvain.chassang@gmail.com>
2020-08-12 09:28:16 -0400

    min_std

--- mb_api/asymptotics.py
+++ mb_api/asymptotics.py
@@ -32,7 +32,8 @@ class PIDMeanAuctionData(AuctionData):
         win_vector.loc[:, 'square_residual'] = \
             np.square(np.dot(centered_wins, weights))
         variance = win_vector.groupby('pid')['square_residual'].mean().mean()
-        return max(1e-5, np.sqrt(variance))
+        min_std = .001/np.sqrt(self.df_auctions.shape[0])
+        return max(min_std, np.sqrt(variance))
 
     def _win_vector(self, df_bids, deviations):
         for rho in deviations:
